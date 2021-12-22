464f1f13aa255ae85b3a54c3add16cc666f22945
whatamelon <whatup@summerclout.dev>
2021-12-22 19:04:44 +0900

    change router pathname error

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
   const router = useRouter();
 
   const goDetail = () => {
-      router.push({ pathname:'/character/index' });
+      router.push({ pathname:'/character' });
   };
   
   return (
